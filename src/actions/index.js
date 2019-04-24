a9851d2196a574dd8f9c6fe7ce01a1ad29d15400
taka <wbspo.bzk@gmail.com>
2019-04-24 19:29:31 -0400

    Added action creators and reducer related to Streams

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,12 @@
 import streams from '../apis/streams';
-import { SIGN_IN, SIGN_OUT } from './types';
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  EDIT_STREAM,
+  DELETE_STREAM } from './types';
 
 export const signIn = (userId, userName) => {
   return {
@@ -15,5 +22,26 @@ export const signOut = () => {
 };
 
 export const createStream = formValues => async dispatch => {
-  streams.post('/streams', formValues);
+  const res = await streams.post('/streams', formValues);
+  dispatch({ type: CREATE_STREAM, payload: res.data });
+};
+
+export const fetchStreams = () => async dispatch => {
+  const res = await streams.get('/stream');
+  dispatch({ type: FETCH_STREAMS, payload: res.data });
+};
+
+export const fetchStream = (id) => async dispatch => {
+  const res = await streams.get(`/streams/:${id}`);
+  dispatch({ type: FETCH_STREAM, payload: res.data });
+};
+
+export const editStream = (id, formValues) => async dispatch => {
+  const res = await streams.put(`/streams/:${id}`, formValues);
+  dispatch({ type: EDIT_STREAM, payload: res.data });
+};
+
+export const deleteStream = (id) => async dispatch => {
+  await streams.delete(`/streams/:${id}`);
+  dispatch({ type: DELETE_STREAM, payload: id });
 };
